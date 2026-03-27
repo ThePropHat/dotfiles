0595df618ea5e0dc671e93fb36002eefaef9ff42
ThePropHat <turntion@gmail.com>
2026-03-27 14:48:24 -0600

    Update random_wallpaper.sh

--- waybar/.config/waybar/random_wallpaper.sh
+++ waybar/.config/waybar/random_wallpaper.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-# Path to your wallpaper folder
 WALL_DIR="$HOME/Desktop/wallpapers"
 
-# 1. Pick a random image
 SELECTED_WALL=$(find "$WALL_DIR" -type f | shuf -n 1)
 
-# 2. Set it with swww (smooth transition)
-swww img "$SELECTED_WALL" --transition-type random --transition-step 1
+awww img "$SELECTED_WALL" --transition-type random --transition-step 1
 
-# 3. Run pywal to update colors
 wal -i "$SELECTED_WALL"
